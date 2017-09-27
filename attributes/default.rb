a6218a691567a588711af3147ef813129fd1f2e2
Suftin, Ivan <isuftin@usgs.gov>
2017-09-27 08:20:01 -0500

    Updated default docker engine to be 17.09.0

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,7 +3,7 @@
 # The properties here are analagous to the service properties for the
 # docker_service properties in the Docker cookbook
 default['owi_docker']['service']['default']['install_method'] = 'package'
-default['owi_docker']['service']['default']['version'] = '17.06.2'
+default['owi_docker']['service']['default']['version'] = '17.09.0'
 
 # Users in this array will be added to the Docker group so they can use the Docker
 # engine without requiring root access
