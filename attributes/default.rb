5393aa63d5c4367b26631f3f02b58f4db2f6b629
Suftin, Ivan <isuftin@usgs.gov>
2017-09-27 08:44:23 -0500

    Update attributes documentation

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,6 +2,10 @@
 
 # The properties here are analagous to the service properties for the
 # docker_service properties in the Docker cookbook
+# See the following:
+# https://github.com/chef-cookbooks/docker/blob/master/libraries/docker_service.rb
+# and
+# https://github.com/chef-cookbooks/docker/blob/master/libraries/docker_service_base.rb
 default['owi_docker']['service']['default']['install_method'] = 'package'
 default['owi_docker']['service']['default']['version'] = '17.09.0'
 
